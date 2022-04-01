88b22ae15b74a3bf9b3816aac527817541f951cf
Sameony <iam2001sam@gmail.com>
2022-04-01 13:06:14 +0530

    feat: backend details addition

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -16,4 +16,13 @@ export const users = [
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
+  {
+    _id: uuid(),
+    firstName: "team",
+    lastName: "B3",
+    email: "b3ToTheTop@gmail.com",
+    password: "b3ToTheTop",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
 ];
