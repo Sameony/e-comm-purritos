88015f993385d6daaa73db2b35952dbd6c6deccf
Sameony <iam2001sam@gmail.com>
2022-04-04 22:05:10 +0530

    feat: increasing and descending sorts available

--- src/components/filters.jsx
+++ src/components/filters.jsx
@@ -3,7 +3,7 @@ import { useFilters } from '../utils';
 const Filters = () => {
     const { state, filterDispatch } = useFilters();
   return (
-    <form className="filters">
+    <aside className="filters fs-md">
             <div className="filterHead">
                 <h2>Filters</h2>
                 <h3 className="btn btn-link-dot" onClick={() => filterDispatch({ type: "RESET" })}>Clear</h3>
@@ -15,7 +15,7 @@ const Filters = () => {
                 
                 <p className='mg-l-sm'>MaxPrice: <span className='snippets mg-l-sm'>{state.maxPrice}</span></p>
             </div>
-            <div className="mg-y-md">
+            <div className="mg-y-md Categories">
                 <h3>Category</h3>
                 <input type="checkbox" className="mg-sm" name="shopping" id="plainBlankets" value="plain-blankets" checked={state.category.includes("plain-blankets")}
                 onChange={(e) =>filterDispatch({ type: "CATEGORY", payload: e.target.value })}/>
@@ -27,7 +27,7 @@ const Filters = () => {
                 onChange={(e) =>filterDispatch({ type: "CATEGORY", payload: e.target.value })} />
                 <label htmlFor="sweaters">Sweaters &#38; hoodies</label> <br />
             </div>
-            <div className="mg-y-md">
+            <div className="mg-y-md Ratings">
                 <h3>Ratings</h3>
                 <input className="mg-sm" type="radio" value="4" checked={state.rating === "4"} name="rating"  onClick={(e) => filterDispatch({ type: "RATINGS", payload:e.target.value })} id="fourStar"  />
                 <label htmlFor="fourStar">4 <i className="fas fa-star"></i> and up</label> <br />
@@ -39,14 +39,18 @@ const Filters = () => {
                 <label htmlFor="oneStar">1 &nbsp;<i className="fas fa-star"></i> and up</label> <br />
             </div>
             
-            <div className="mg-y-md">
+            <div className="mg-y-md Sorting">
                 <h3>Sort By</h3>
-                <input name="sort" checked={state.sort === "PRICE"} className="mg-sm" type="radio" value="PRICE"  id="sortByPrice" onClick={(e) => filterDispatch({ type: "SORT", payload: e.target.value })} />
-                <label htmlFor="sortByPrice">Price</label> <br />
-                <input className="mg-sm" type="radio" checked={state.sort === "RATING"} name="sort" value="RATING" id="sortByRating" onClick={(e) => filterDispatch({ type: "SORT", payload: e.target.value })}  />
-                <label htmlFor="sortByRating">Ratings</label> <br />
+                <input name="sort" checked={state.sort === "PRICE_A"} className="mg-sm" type="radio" value="PRICE_A"  id="sortByPrice" onClick={(e) => filterDispatch({ type: "SORT", payload: e.target.value })} />
+                <label htmlFor="sortByPrice">Price(Low to High)</label> <br />
+                <input name="sort" checked={state.sort === "PRICE_D"} className="mg-sm" type="radio" value="PRICE_D"  id="sortByPrice" onClick={(e) => filterDispatch({ type: "SORT", payload: e.target.value })} />
+                <label htmlFor="sortByPrice">Price(High to Low)</label> <br />
+                <input className="mg-sm" type="radio" checked={state.sort === "RATING_A"} name="sort" value="RATING_A" id="sortByRating" onClick={(e) => filterDispatch({ type: "SORT", payload: e.target.value })}  />
+                <label htmlFor="sortByRating">Ratings(Low to High)</label> <br />
+                <input className="mg-sm" type="radio" checked={state.sort === "RATING_D"} name="sort" value="RATING_D" id="sortByRating" onClick={(e) => filterDispatch({ type: "SORT", payload: e.target.value })}  />
+                <label htmlFor="sortByRating">Ratings(High to Low)</label> <br />
             </div>
-        </form>
+        </aside>
   )
 }
 
--- src/utils/filterComposition.js
+++ src/utils/filterComposition.js
@@ -12,20 +12,26 @@ export const pricingFunction = (state, prodList) => {
 
 export const sortprodList = (state, prodList) => {
   switch (state.sort) {
-    case "PRICE":
+    case "PRICE_A":
       return [...prodList].sort((a, b) => Number(a.price) - Number(b.price));
-    case "RATING":
+    case "PRICE_D":
+      return [...prodList].sort((a, b) => Number(b.price) - Number(a.price));
+    case "RATING_A":
+      return [...prodList].sort((a, b) => Number(a.rating) - Number(b.rating));
+    case "RATING_D":
       return [...prodList].sort((a, b) => Number(b.rating) - Number(a.rating));
     default:
       return prodList;
   }
 };
 
-export const CategoryHandler= (state, prodList) => {
+export const CategoryHandler = (state, prodList) => {
   const { category } = state;
   if (category.length === 0) {
     return prodList;
   } else {
-    return prodList.filter((product) => category.includes(product.categoryName));
+    return prodList.filter((product) =>
+      category.includes(product.categoryName)
+    );
   }
 };
