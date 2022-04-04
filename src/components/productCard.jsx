214d7c4d878abee5fe555c098d3302c563643b43
Sameony <iam2001sam@gmail.com>
2022-04-04 18:39:11 +0530

    style: altered card component and layout

--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -7,27 +7,33 @@ const ProductCard = ({cardTitle, cardReviews, cardRating, Price, prevPrice, img}
     const stars = hasDecimal?roundedWhole+1:roundedWhole;
   return (
     <div className="productCard pd-sm mg-sm">
-                <img src={img} alt="red blanket for a cat" />
+                <div className='productImage flex align-items-center'><img src={img} alt={img} /></div>
                 <div className="cardContent">
                     <div className="priceContainer mg-y-sm pd-sm">
                         <h3 className="cardHead">{cardTitle}</h3>
                         <div className="rateMe prim-color">
-                            {[...Array(roundedWhole)].map((e, i) => <i className="fas fa-star" key={i}></i>)}
-                            {hasDecimal?<i class="fas fa-star-half-alt"></i>:``}
+                            {/* complete stars */}
+                            {[...Array(roundedWhole)].map((e, i) => <i className="fas fa-star" key={i}></i>)}  
+                            {/* half stars */}
+                            {hasDecimal?<i className="fas fa-star-half-alt"></i>:``}
+                            {/* empty stars */}
                             {[...Array(5-stars)].map((e, i) => <i className="far fa-star" key={i}></i>)}
-                            <span>({cardReviews} Reviews)</span>
+                                <span className='mg-x-sm fw-lg'>{cardRating}</span>
                         </div>
+                        <span>({cardReviews} Reviews)</span>
                         <div className="productPrice fw-xxl"><i className="fas fa-rupee-sign"></i> {Price}
                             {prevPrice?<strike className="fs-sm"><i className="fas fa-rupee-sign mg-l-sm"></i>{prevPrice}</strike>:``}
                         </div>
 
                     </div>
                 </div>
-                <div className="filterHead">
-                    <button className="btn btn-success mg-y-sm"><i className="fas fa-shopping-cart mg-r-sm"></i>Add to
+                
+                <div className='cardActionButtons'>
+                <button className="btn btn-success fs-md mg-t-sm"><i className="fas fa-shopping-cart mg-r-sm"></i>Add to
                         Cart</button>
-                    <button className="btn btn-primary mg-y-sm"><i className="fas fa-heart mg-r-sm"></i> Wishlist</button>
                 </div>
+                    
+                    
 
             </div>
   )
